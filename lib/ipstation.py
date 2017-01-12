b763b0f34138bdbb9a8a84d14674710f9b6f5baa
level077 <level077@163.com>
2017-01-12 16:54:35 +0800

    remove some code

--- lib/ipstation.py
+++ lib/ipstation.py
@@ -9,55 +9,7 @@ _unpack_V = lambda b: struct.unpack("<L", b)
 _unpack_N = lambda b: struct.unpack(">L", b)
 _unpack_C = lambda b: struct.unpack("B", b)
 
-class IP:
-    offset = 0
-    index = 0
-    binary = ""
-
-    @staticmethod
-    def load(file):
-        try:
-            path = os.path.abspath(file)
-            with open(path, "rb") as f:
-                IP.binary = f.read()
-                IP.offset, = _unpack_N(IP.binary[:4])
-                IP.index = IP.binary[4:IP.offset]
-        except Exception as ex:
-            print "cannot open file %s" % file
-            print ex.message
-            exit(0)
-
-    @staticmethod
-    def find(ip):
-        index = IP.index
-        offset = IP.offset
-        binary = IP.binary
-        nip = inet_aton(ip)
-        ipdot = ip.split('.')
-        if int(ipdot[0]) < 0 or int(ipdot[0]) > 255 or len(ipdot) != 4:
-            return "N/A"
-
-        tmp_offset = int(ipdot[0]) * 4
-        start, = _unpack_V(index[tmp_offset:tmp_offset + 4])
-
-        index_offset = index_length = 0
-        max_comp_len = offset - 1028
-        start = start * 8 + 1024
-        while start < max_comp_len:
-            if index[start:start + 4] >= nip:
-                index_offset, = _unpack_V(index[start + 4:start + 7] + chr(0).encode('utf-8'))
-                index_length, = _unpack_C(index[start + 7])
-                break
-            start += 8
-
-        if index_offset == 0:
-            return "N/A"
-
-        res_offset = offset + index_offset - 1024
-        return binary[res_offset:res_offset + index_length].decode('utf-8')
-
-
-class IPX:
+class IPSTATION:
     binary = ""
     index = 0
     offset = 0
@@ -67,9 +19,9 @@ class IPX:
         try:
             path = os.path.abspath(file)
             with open(path, "rb") as f:
-                IPX.binary = f.read()
-                IPX.offset, = _unpack_N(IPX.binary[:4])
-                IPX.index = IPX.binary[4:IPX.offset]
+                IPSTATION.binary = f.read()
+                IPSTATION.offset, = _unpack_N(IPSTATION.binary[:4])
+                IPSTATION.index = IPSTATION.binary[4:IPSTATION.offset]
         except Exception as ex:
             print "cannot open file %s" % file
             print ex.message
@@ -77,9 +29,9 @@ class IPX:
 
     @staticmethod
     def find(ip):
-        index = IPX.index
-        offset = IPX.offset
-        binary = IPX.binary
+        index = IPSTATION.index
+        offset = IPSTATION.offset
+        binary = IPSTATION.binary
         nip = inet_aton(ip)
         ipdot = ip.split('.')
         if int(ipdot[0]) < 0 or int(ipdot[0]) > 255 or len(ipdot) != 4:
